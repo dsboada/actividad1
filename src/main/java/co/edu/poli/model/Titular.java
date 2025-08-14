c62508cab650a3fb4bd05f77b407b6b2da401b22
dplazas <dplazas@poligran.edu.co>
2025-08-13 21:37:41 -0500

    Titular y Visa constructor, get set

--- src/main/java/co/edu/poli/model/Titular.java
+++ src/main/java/co/edu/poli/model/Titular.java
@@ -8,6 +8,36 @@ import java.util.*;
  */
 public class Titular {
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public Titular(String id, String nombre, String fechaNac) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+    }
+
     /**
      * Default constructor
      */
--- src/main/java/co/edu/poli/model/Visa.java
+++ src/main/java/co/edu/poli/model/Visa.java
@@ -8,6 +8,45 @@ import java.util.*;
  */
 public class Visa {
 
+    public Visa(String num, Pais pais, int mulEntry, Pasaporte pasaporte) {
+        this.num = num;
+        this.pais = pais;
+        this.mulEntry = mulEntry;
+        this.pasaporte = pasaporte;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public int getMulEntry() {
+        return mulEntry;
+    }
+
+    public void setMulEntry(int mulEntry) {
+        this.mulEntry = mulEntry;
+    }
+
+    public Pasaporte getPasaporte() {
+        return pasaporte;
+    }
+
+    public void setPasaporte(Pasaporte pasaporte) {
+        this.pasaporte = pasaporte;
+    }
+
     /**
      * Default constructor
      */
