f13bd35c0e74ef8bcfbb82c16251bcf594e1acfa
dplazas <dplazas@poligran.edu.co>
2025-08-13 21:23:44 -0500

    Pais constructor, get set

--- src/main/java/co/edu/poli/model/Pais.java
+++ src/main/java/co/edu/poli/model/Pais.java
@@ -8,6 +8,36 @@ import java.util.*;
  */
 public class Pais {
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Ciudad> getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(List<Ciudad> ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Pais(String codigo, String nombre, List<Ciudad> ciudad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+    }
+
     /**
      * Default constructor
      */
