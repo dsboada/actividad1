34bb454a8c2ff939a0a51ccf7248d75f5331604b
dplazas <dplazas@poligran.edu.co>
2025-08-13 21:33:14 -0500

    Pasaporte constructor, get set

--- src/main/java/co/edu/poli/model/Pasaporte.java
+++ src/main/java/co/edu/poli/model/Pasaporte.java
@@ -8,6 +8,45 @@ import java.util.*;
  */
 public class Pasaporte {
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFechaExp() {
+        return fechaExp;
+    }
+
+    public void setFechaExp(String fechaExp) {
+        this.fechaExp = fechaExp;
+    }
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public Pasaporte(String id, String fechaExp, Titular titular, Pais pais) {
+        this.id = id;
+        this.fechaExp = fechaExp;
+        this.titular = titular;
+        this.pais = pais;
+    }
+
     /**
      * Default constructor
      */
